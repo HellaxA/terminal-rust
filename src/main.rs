963aa71273c09a1b766faafe22266f4ebc565dbf
Hellax <abereznikov@gmail.com>
2026-05-17 20:35:05 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,17 @@ fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
+
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let input = input.trim();
 
+        if input.eq("exit") {
+            break;
+        }
+
         println!("{input}: command not found");
     }
 }
+
+fn parseInput() {}
