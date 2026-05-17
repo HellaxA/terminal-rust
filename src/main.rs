3a393bff6aa864235834fb1882aa8c0cb0cbf3db
Hellax <abereznikov@gmail.com>
2026-05-17 18:13:50 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
+
+    println!("{input}: command not found");
 }
