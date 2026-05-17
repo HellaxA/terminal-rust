df660e1cde90b660c81ef55a53760038cf10b24b
Hellax <abereznikov@gmail.com>
2026-05-17 23:04:36 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     loop {
@@ -10,12 +11,32 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
         let input = input.trim();
 
-        if input.eq("exit") {
-            break;
-        }
-
-        println!("{input}: command not found");
+        process_input(input);
     }
 }
 
-fn parseInput() {}
+fn process_input(input: &str) {
+    let mut split_input = input.split_whitespace();
+    let command = split_input.next().unwrap_or("invalid");
+
+    match command {
+        "exit" => {
+            process::exit(0);
+        }
+        "echo" => {
+            echo(input);
+        }
+        _ => {
+            println!("{input}: command not found");
+        }
+    }
+}
+
+fn echo(input: &str) {
+    if input.len() <= 4 {
+        println!("{input}: command not found");
+        return;
+    }
+    let input = &input[5..];
+    println!("{input}");
+}
